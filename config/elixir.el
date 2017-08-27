a82a3900361ad24aa38524539ef8fb51a4b546f1
Kevin Keen <kevin@coobii.com>
2017-08-27 22:58:00 +0800

    Sun Aug 27 22:58:53 CST 2017

--- config/elixir.el
+++ config/elixir.el
@@ -11,6 +11,6 @@
 ;; (getenv "PATH")
 
 (setq alchemist-mix-command "~/elixir-1.5.1/bin/mix")
-(setq alchemist-iex-program-name "~/elixir-1.5.1/bin/iex -S phoenix.server")
+(setq alchemist-iex-program-name "~/elixir-1.5.1/bin/iex")
 (setq alchemist-execute-command "~/elixir-1.5.1/bin/elixir")
 (setq alchemist-compile-command "~/elixir-1.5.1/bin/elixirc")
