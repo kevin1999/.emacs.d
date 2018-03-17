84e34b593b6607904acc5602c3b2d52ef07b1d7f
Kevin Keen <kevin@coobii.com>
2018-03-17 15:36:13 +0800

    Update on Sat Mar 17 15:36:13 CST 2018

--- config/prolog.el
+++ config/prolog.el
@@ -1,11 +1,15 @@
 (autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
 (autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
 (autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
-(setq prolog-system 'swi)  ; optional, the system you are using;
-                           ; see `prolog-system' below for possible values
+
+(setq prolog-system 'swi
+      prolog-program-switches '((swi ("-G128M" "-T128M" "-L128M" "-O"))
+                                (t nil))
+      prolog-electric-if-then-else-flag t)
+
 (setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
                                 ("\\.m$" . mercury-mode))
-                               auto-mode-alist))
+                              auto-mode-alist))
 
 (require 'ediprolog)
 
